08c58970ade5a8b5c5bf844a6d595ae5cd79cb15
Matthew Adams <matthew.adams@northscaler.com>
2020-11-12 13:07:23 -0600

    update deps

--- lic.js
+++ lic.js
@@ -3,6 +3,7 @@
 const fs = require('fs')
 const _ = { template: require('lodash.template') }
 
+// eslint-disable-next-line node/no-path-concat
 const tmpl = _.template(fs.readFileSync(`${__dirname}/LICENSE.tmpl`))
 const lic = tmpl({ YEAR: new Date().getFullYear(), HOLDER: 'Northscaler, Inc.' })
 
